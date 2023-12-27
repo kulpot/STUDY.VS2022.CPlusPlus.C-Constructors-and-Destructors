6f89f86967b21c0b54e335cf8a1005d6edae9c6c
Kulpot <kulpotwahu@gmail.com>
2023-12-28 02:08:40 +0800

    ....

--- C++ Constructors and Destructors/CppScratchPad.cpp
+++ C++ Constructors and Destructors/CppScratchPad.cpp
@@ -4,24 +4,55 @@ using std::cout;
 using std::string;
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // -------------------------------- C++ Classes --------------------------------
 // ref link:https://www.youtube.com/watch?v=a9R9mP3oJBY&list=PLRwVmtr-pp05LyV3bYHwrFacNSNjbUqS6
 
 // C++ Classes - 
 
-class Person
-{
-public:
-	string firstName;
-	string lastName;
-//private:
-};
-
-void main()
-{	
-	//Person person = new Person();		// C# or Java
-	Person person;		// "person" object is in stack instead on the heap
-	person.firstName = "Kulpot";
-	person.lastName = "King";
-	cout << person.firstName << " " << person.lastName;
-}
+//class Person
+//{
+//public:
+//	string firstName;
+//	string lastName;
+////private:
+//};
+//
+//void main()
+//{	
+//	//Person person = new Person();		// C# or Java
+//	Person person;		// "person" object is in stack instead on the heap
+//	person.firstName = "Kulpot";
+//	person.lastName = "King";
+//	cout << person.firstName << " " << person.lastName;
+//}
